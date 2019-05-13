53f7d031fd948820efa2c9f3b0f1daf0f648ed1a
Jure Matoh <matohjure@hotmail.com>
2019-05-13 14:20:03 +0100

    Fixed jpegSignature for streamCamera.takeImage to work

--- src/lib/stream-camera.ts
+++ src/lib/stream-camera.ts
@@ -37,7 +37,7 @@ declare interface StreamCamera {
 
 class StreamCamera extends EventEmitter {
 
-	static readonly jpegSignature = Buffer.from([0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46, 0x00]);
+	static readonly jpegSignature = Buffer.from([0xFF, 0xD8, 0xFF, 0xDB, 0x00, 0x84, 0x00]);
 
 	private options: StreamOptions;
 	private childProcess?: ChildProcess;
