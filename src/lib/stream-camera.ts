cd8e9871c8f321d7466e85f48e14b8da8de728dc
Haas <christiaan@pineapplestudios.nl>
2020-06-21 17:59:27 +0200

    Added support for Pi Zero

--- src/lib/stream-camera.ts
+++ src/lib/stream-camera.ts
@@ -61,6 +61,8 @@ class StreamCamera extends EventEmitter {
       case 'BCM2835 - Pi 3 Model B':
       case 'BCM2835 - Pi 3 Model B+':
       case 'BCM2835 - Pi 4 Model B':
+      case 'BCM2835 - Pi Zero':
+      case 'BCM2835 - Pi Zero W':
         return Buffer.from([0xff, 0xd8, 0xff, 0xdb, 0x00, 0x84, 0x00]);
       default:
         throw new Error(
